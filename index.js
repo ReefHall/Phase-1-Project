2b3edfb3435fbf7569c4a1d2aaa11c0d22293e8f
zinakalu <zina.kalu@gmail.com>
2023-03-08 14:13:18 -0500

    created css

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
-// fetch("http://localhost:3000/BootCamps")
-// .then(response => response.json())
-// .then(bootCamps => bootCamps.forEach(bootCamp => renderBootCamp(bootCamp)))
-
-// function renderBootCamp(bootCamp) {
-//     const img = document.getElementById('flat-iron')
-//     const name = document.getElementById('name')
-//     const rating = document.getElementById('rating')
-//     const comment = document.getElementById('comment')
-
-//     img.src = bootCamp.image
-//     name.innerText = bootCamp.name
-//     rating.innerText = bootCamp.rating
-//     comment.innerText = bootCamp.comment
-
-   
-
-// }
-
-//we want to get the name, image, rating, comment, + website
 
 const banner = document.querySelector('.banner')
 
 
 fetch("http://localhost:3000/BootCamps")
-.then(res => res.json())
-.then(BootCamps => BootCamps.forEach(bootcamp => renderBootCamp(bootcamp)))
+    .then(res => res.json())
+    .then(BootCamps => BootCamps.forEach(bootcamp => renderBootCamp(bootcamp)))
 
 
-function renderBootCamp(bootcamp){
-const divElement = document.createElement('div')
-const bootcampImg = document.createElement('img')
-// const bootcampName = document.createElement('h2')
-const bootcampRating = document.createElement('p')
-const bootcampComment = document.createElement('p')
+function renderBootCamp(bootcamp) {
+    const divElement = document.createElement('div')
+    const divElementTwo = document.createElement('div')
+    const bootcampImg = document.createElement('img')
+    // const bootcampName = document.createElement('h2')
+    const bootcampRating = document.createElement('p')
+    const bootcampComment = document.createElement('p')
 
-bootcampImg.src = bootcamp.image
-// bootcampName.textContent = bootcamp.name
-bootcampRating.textContent = bootcamp.rating
-bootcampComment.textContent = bootcamp.comment
-bootcampImg.id = "img"
-divElement.id = "DivElement"
+    bootcampImg.src = bootcamp.image
+    // bootcampName.textContent = bootcamp.name
+    bootcampRating.textContent = `Rating: ${bootcamp.rating}`
+    bootcampComment.textContent = `Comments: ${bootcamp.comment}`
+    bootcampImg.id = "img"
+    divElement.id = "DivElement"
+    divElementTwo.id = "DivElementTwo"
 
-divElement.append(bootcampImg, bootcampRating, bootcampComment)
-banner.append(divElement)
-
+    divElement.append(bootcampImg)
+    divElement.append(divElementTwo)
+    divElementTwo.append(bootcampRating, bootcampComment)
+    banner.append(divElement)
 
 
 }
@@ -58,12 +41,13 @@ function submitForm(e) {
     const inPutWebsite = document.getElementById('website')
     const inputImage = document.getElementById('image')
 
-    const newObj = {name: inPutName.value,
-                    image: inputImage.value,
-                    rating: inPutRating.value,
-                    comment: inPutComment.value,
-                    website: inPutRating.value
-                   }
+    const newObj = {
+        name: inPutName.value,
+        image: inputImage.value,
+        rating: inPutRating.value,
+        comment: inPutComment.value,
+        website: inPutRating.value
+    }
     fetch('http://localhost:3000/BootCamps', {
         method: 'POST',
         headers: {
@@ -71,10 +55,10 @@ function submitForm(e) {
         },
         body: JSON.stringify(newObj)
     })
-      .then(response => response.json())
-      .then(obj => renderBootCamp(obj))
-                    
-banner.style.grid_template_columns="1fr";
+        .then(response => response.json())
+        .then(obj => renderBootCamp(obj))
+
+    banner.style.grid_template_columns = "1fr";
 
 }
 
