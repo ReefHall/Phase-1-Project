fda3e0179443790eee08d2da590bfbcca4574c13
zinakalu <zina.kalu@gmail.com>
2023-03-08 10:24:37 -0500

    added elements

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-fetch("http://localhost:3000/BootCamps")
-.then(response => response.json())
-.then(bootCamps => bootCamps.forEach(bootCamp => renderBootCamp(bootCamp)))
+// fetch("http://localhost:3000/BootCamps")
+// .then(response => response.json())
+// .then(bootCamps => bootCamps.forEach(bootCamp => renderBootCamp(bootCamp)))
 
-function renderBootCamp(bootCamp) {
-    //const divBoot = document.createElement('div')
-    const img = document.getElementById('flat-iron')
-    const name = document.getElementById('name')
-    const rating = document.getElementById('rating')
-    const comment = document.getElementById('comment')
+// function renderBootCamp(bootCamp) {
+//     const img = document.getElementById('flat-iron')
+//     const name = document.getElementById('name')
+//     const rating = document.getElementById('rating')
+//     const comment = document.getElementById('comment')
 
-    img.src = bootCamp.image
-    name.innerText = bootCamp.name
-    rating.innerText = bootCamp.rating
-    comment.innerText = bootCamp.comment
+//     img.src = bootCamp.image
+//     name.innerText = bootCamp.name
+//     rating.innerText = bootCamp.rating
+//     comment.innerText = bootCamp.comment
 
    
+
+// }
+
+//we want to get the name, image, rating, comment, + website
+
+const banner = document.querySelector('.banner')
+
+
+fetch("http://localhost:3000/BootCamps")
+.then(res => res.json())
+.then(BootCamps => BootCamps.forEach(bootcamp => renderBootCamp(bootcamp)))
+
+
+function renderBootCamp(bootcamp){
+const bootcampImg = document.createElement('img')
+// const bootcampName = document.createElement('h2')
+const bootcampRating = document.createElement('p')
+const bootcampComment = document.createElement('p')
+
+bootcampImg.src = bootcamp.image
+// bootcampName.textContent = bootcamp.name
+bootcampRating.textContent = bootcamp.rating
+bootcampComment.textContent = bootcamp.comment
+bootcampImg.id = "img"
+
+banner.append(bootcampImg, bootcampRating, bootcampComment)
+
 
 }
